7399391e2bb62170358bebc39281f7d34b06101a
twkim <twkim9475@gmail.com>
2023-09-09 18:49:57 +0900

    매장안내 지도 추가

--- src/views/web/section/consulting.js
+++ src/views/web/section/consulting.js
@@ -1,37 +1,113 @@
 import { CONSENT } from "const/consent";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { styled } from "styled-components";
 import { Checkbox } from "../components/checkbox";
 
 const Consulting = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    phone: "",
+    location: "",
+    question: "",
+  });
+
   const [checked, setChecked] = useState(false);
-  const [comment, setComment] = useState("");
+
+  const onChangeData = (e, setData) => {
+    const { name, value } = e.currentTarget;
+    setData((preVal) => ({ ...preVal, [name]: value }));
+  };
+
+  const titleRef = useRef(null);
+  const subtitleRef = useRef(null);
+  const formRef = useRef(null);
+  const [scrollActive, setScrollActive] = useState(false);
+  const callback = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting && !scrollActive) return setScrollActive(true);
+      const target = entry.target;
+      if (target.classList.contains("active")) return;
+
+      if (target.classList.contains("title")) {
+        const titleEl = document.querySelector("#consulting .title");
+        return titleEl.classList.add("active");
+      }
+      if (target.classList.contains("subtitle")) {
+        const subtitleEl = document.querySelector("#consulting .subtitle");
+        return subtitleEl.classList.add("active");
+      }
+      if (target.classList.contains("consulting__wrapper")) {
+        const consultingWrapperEl = document.querySelector(
+          "#consulting .consulting__wrapper"
+        );
+        return consultingWrapperEl.classList.add("active");
+      }
+    });
+  };
+
+  useEffect(() => {
+    if (!titleRef.current || !subtitleRef.current || !formRef.current) return;
+
+    const options = { root: null, threshold: 0.5 };
+    const io = new IntersectionObserver(callback, options);
+    io.observe(titleRef.current);
+    io.observe(subtitleRef.current);
+    io.observe(formRef.current);
+    return () => {
+      io.disconnect();
+    };
+  }, []);
 
   return (
     <section id="consulting">
       <div className="container">
-        <h1 className="title">가맹상담 신청</h1>
-        <h4 className="subtitle">
+        <h1 className="title" ref={titleRef}>
+          가맹상담 신청
+        </h1>
+        <h4 className="subtitle" ref={subtitleRef}>
           성공적인 창업의 시작 도시맥주에서 시작하세요!
         </h4>
         <hr />
-        <div className="consulting__wrapper">
+        <div className="consulting__wrapper" ref={formRef}>
           <div className="consulting__form">
             <div className="input__wrapper">
               <div>성함</div>
-              <input></input>
+              <input
+                name="name"
+                value={formData.name}
+                onChange={(e) => {
+                  onChangeData(e, setFormData);
+                }}
+              ></input>
             </div>
             <div className="input__wrapper">
               <div>연락처</div>
-              <input></input>
+              <input
+                name="phone"
+                value={formData.phone}
+                onChange={(e) => {
+                  onChangeData(e, setFormData);
+                }}
+              ></input>
             </div>
             <div className="input__wrapper">
               <div>창업희망지역</div>
-              <input></input>
+              <input
+                name="location"
+                value={formData.location}
+                onChange={(e) => {
+                  onChangeData(e, setFormData);
+                }}
+              ></input>
             </div>
             <div className="input__wrapper">
               <div>추가문의사항</div>
-              <FormTextareaStyled value={comment}></FormTextareaStyled>
+              <FormTextareaStyled
+                name="question"
+                onChange={(e) => {
+                  onChangeData(e, setFormData);
+                }}
+              ></FormTextareaStyled>
             </div>
           </div>
           <div className="consulting__btn_wrapper">
@@ -42,7 +118,14 @@ const Consulting = () => {
             <Checkbox checked={checked} onChange={setChecked}>
               <span>위의 ‘개인정보의 제공 및 활용 동의서’ 에 동의합니다.</span>
             </Checkbox>
-            <button>문의하기</button>
+            <button
+              onClick={() => {
+                console.log(formData);
+                console.log(checked);
+              }}
+            >
+              문의하기
+            </button>
           </div>
         </div>
       </div>
--- src/views/web/section/store.js
+++ src/views/web/section/store.js
@@ -4,6 +4,8 @@ import dummy3 from "assets/dummy/dummy_3.jpg";
 import dummy4 from "assets/dummy/dummy_4.jpg";
 import { useEffect, useRef, useState } from "react";
 
+const { kakao } = window;
+
 const Store = () => {
   const elementRef = useRef(null);
   const [scrollActive, setScrollActive] = useState(false);
@@ -28,12 +30,70 @@ const Store = () => {
     };
   }, []);
 
+  useEffect(() => {
+    // 마커를 클릭하면 장소명을 표출할 인포윈도우 입니다
+    var infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
+    const mapContainer = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
+    const mapOption = {
+      //지도를 생성할 때 필요한 기본 옵션
+      center: new kakao.maps.LatLng(37.5259628, 127.0376468), //지도의 중심좌표.
+      level: 2, //지도의 레벨(확대, 축소 정도)
+    };
+
+    const map = new kakao.maps.Map(mapContainer, mapOption); //지도 생성 및 객체 리턴
+
+    // 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+    var mapTypeControl = new kakao.maps.MapTypeControl();
+    // 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+    // kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+    map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+    // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+    var zoomControl = new kakao.maps.ZoomControl();
+    map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+    // 마커를 표시할 위치와 내용을 가지고 있는 객체 배열입니다
+    var positions = [
+      {
+        content:
+          "<div class='marker_info_wrapper'><h4>도시맥주 압구정점</h4><hr/><h6>서울 강남구 선릉로 157길 23-4 ,1층</h6></div>",
+        latlng: new kakao.maps.LatLng(37.5259628, 127.0376468),
+      },
+    ];
+
+    for (var i = 0; i < positions.length; i++) {
+      // 마커를 생성합니다
+      var marker = new kakao.maps.Marker({
+        map: map, // 마커를 표시할 지도
+        position: positions[i].latlng, // 마커의 위치
+      });
+
+      // 마커에 표시할 인포윈도우를 생성합니다
+      var infowindow = new kakao.maps.InfoWindow({
+        content: positions[i].content, // 인포윈도우에 표시할 내용
+      });
+
+      // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
+      // 이벤트 리스너로는 클로저를 만들어 등록합니다
+      // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
+      kakao.maps.event.addListener(
+        marker,
+        "click",
+        makeOverListener(map, marker, infowindow)
+      );
+    }
+    // 인포윈도우를 표시하는 클로저를 만드는 함수입니다
+    function makeOverListener(map, marker, infowindow) {
+      return function () {
+        infowindow.open(map, marker);
+      };
+    }
+  }, []);
   return (
     <>
       <section id="store" ref={elementRef}>
         <div className="store__wrapper container">
           <h1 className="title">매장안내</h1>
-          <div className="store__map">지도</div>
+          <div id="map"></div>
         </div>
       </section>
       <section id="open">
@@ -45,6 +105,13 @@ const Store = () => {
                 <h3>도시맥주 압구정점(9월 오픈예정)</h3>
                 <h6>서울 강남구 선릉로 157길 23-4 ,1층</h6>
               </div>
+            </div>
+            <div className="open_store__item __empty">
+              <h1>WHO'S NEXT?</h1>
+              {/* <div className="item__discription">
+                <h3>도시맥주 압구정점(9월 오픈예정)</h3>
+                <h6>서울 강남구 선릉로 157길 23-4 ,1층</h6>
+              </div> */}
             </div>
             {/* <div className="open_store__item">
               <img src={dummy3}></img>
